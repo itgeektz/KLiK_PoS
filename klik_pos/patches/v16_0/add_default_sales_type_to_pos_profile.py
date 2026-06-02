147c203ec455b40c8c8a18136c67896a66b092f6
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-06-02 20:48:18 +0300

    chore: rearrange custom klik pos fields in POS Profile

--- klik_pos/patches/v16_0/add_default_sales_type_to_pos_profile.py
+++ klik_pos/patches/v16_0/add_default_sales_type_to_pos_profile.py
@@ -1,29 +1,8 @@
 import frappe
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-
 
 FIELDNAME = "default_sales_type"
 
-
 def execute():
-    custom_fields = {
-        "POS Profile": [
-            {
-                "fieldname": FIELDNAME,
-                "label": "Default Sales Type",
-                "fieldtype": "Select",
-                "options": "Cash\nCredit",
-                "default": "Cash",
-                "insert_after": "allow_partial_payment",
-                "in_list_view": 0,
-                "read_only": 0,
-                "reqd": 0,
-            }
-        ]
-    }
-
-    create_custom_fields(custom_fields, update=True)
-
     if frappe.db.has_column("POS Profile", FIELDNAME):
         frappe.db.sql(
             """

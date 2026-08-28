6096661f6697c978b154183f85a46231ac604fed
itgeektz <159551039+itgeektz@users.noreply.github.com>
2026-08-28 04:49:32 +0300

    Add custom_allow_out_of_stock_sale property

--- klik_spa/src/stores/posProfileStore.ts
+++ klik_spa/src/stores/posProfileStore.ts
@@ -35,6 +35,7 @@ export interface POSProfile {
   custom_delivery_charge_account?: string;
   custom_prevent_invoice_reprinting?: boolean | number;
   custom_allow_return?: boolean | number | string;
+  custom_allow_out_of_stock_sale?: boolean | number;
   custom_allow_to_create_and_edit_customers?: number;
   custom_default_view?: "Grid View" | "List View";
   custom_cart_item_insertion_position?: "Top" | "Bottom";

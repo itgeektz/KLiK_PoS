9a3f42d3b54a4af49d1f77619827ba9e212b4ab7
itgeektz <159551039+itgeektz@users.noreply.github.com>
2026-08-28 04:51:36 +0300

    Update out-of-stock logic in MenuItemCard

--- klik_spa/src/components/MenuItemCard.tsx
+++ klik_spa/src/components/MenuItemCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import type { MenuItem } from "../../types"
+import { usePOSProfileStore } from "../stores/posProfileStore"
 
 interface MenuItemCardProps {
   item: MenuItem
@@ -9,7 +10,8 @@ interface MenuItemCardProps {
 
 export default function MenuItemCard({ item, onAddToCart }: MenuItemCardProps) {
   const isServiceItem = item.is_stock_item === false
-  const isOutOfStock = item.is_stock_item !== false && item.available <= 0
+  const isOutOfStock = item.is_stock_item !== false && item.available <= 0 &&
+    !usePOSProfileStore.getState().posDetails?.custom_allow_out_of_stock_sale
 
   return (
     <div
